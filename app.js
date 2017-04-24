56aa88a8baa36daafce92f394fe2c04701a0c8f2
Sergiy Kaliberda <kaliberdasergiy@yandex.ru>
2017-04-24 21:41:08 +0300

    Intermediate commit for the Name Calculator

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 (function () {
   'use strict';
-  angular.module('myFirstApp', [])
-.controller('MyFirstController', function($scope){
-  $scope.name = "sergo";
-  $scope.sayHello = function(){
-    return "cursera";
-  };
+
+  angular.module('NameCalculator', [])
+  .controller('NameCalculatorController', function ($scope){
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function () {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+    };
   });
 })();
