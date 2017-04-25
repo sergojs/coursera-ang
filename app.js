c317be86a5d6f189390c7c6995fc013bfd5bd456
Sergiy Kaliberda <kaliberdasergiy@yandex.ru>
2017-04-25 11:50:30 +0300

    Implemented Upercase function

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 (function () {
   'use strict';
 
-  angular.module('NameCalculator', [])
-  .controller('NameCalculatorController', function ($scope){
-    $scope.name = "";
-    $scope.totalValue = 0;
-    $scope.displayNumeric = function () {
-      var totalNameValue =  calculateNumericForString($scope.name);
-      $scope.totalValue = totalNameValue;
-    };
-    function calculateNumericForString(string) {
-      var totalStringValue = 0;
-    
-      for (var i = 0; i < string.length; i++) {
-        totalStringValue += string.charCodeAt(i);
-      }
-      return totalStringValue;
-    };
+  angular.module('DIApp', [])
+  .controller('DIController', DIController);
 
-  });
+  function DIController ($scope, $filter, $injector){
+    $scope.name = "Sergo";
+
+    $scope.upper = function () {
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+    console.log($injector.annotate(DIController));
+  }
+
+  function AnnotateMe(name, job, blah){
+    return "Blah!";
+  }
+  // console.log($injector.annotate(DIController));
 })();
